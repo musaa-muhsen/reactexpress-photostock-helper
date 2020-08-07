f5adfb9ca76a64bd7b24265ac4a57164522d62ec
musaa-muhsen <musaamuhsen@gmail.com>
2020-08-07 11:14:27 +0100

    all before deploy

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
+//require('dotenv').config()
+
+//console.log(process.env)
 
 //app.get('/api/images', async (req, res) => {
 
 app.get('/api/:word', async (req, res) => {
 
-  const API_KEY = 'BgwLpezqvGtqHU5Q23sXjv0kJSebWvOoG_3fKA6ALOs';
+  //const API_KEY = process.env.API_KEY;
   const word = req.params.word;
   console.log(word);
    
   // res.json(customers);
  // const imageURL = await fetch(`https://api.unsplash.com/search/photos?query=${word}&client_id=BgwLpezqvGtqHU5Q23sXjv0kJSebWvOoG_3fKA6ALOs`); 
-  const imageURL = await fetch(`https://api.unsplash.com/search/photos?per_page=30&query=${word}&client_id=${API_KEY}`); 
+  const imageURL = await fetch(`https://api.unsplash.com/search/photos?per_page=30&query=${word}&client_id=BgwLpezqvGtqHU5Q23sXjv0kJSebWvOoG_3fKA6ALOs`); 
 
    const data = await imageURL.json();
    res.json(data)
@@ -21,11 +24,11 @@ app.get('/api/:word', async (req, res) => {
 
 });
 
-const port = 5004;
+const port = 5200;
 
 app.listen(port, () => `Server running on port ${port}`);
 
-console.log(process.env.PORT)
+//console.log(process.env.PORT)
   // const customers = [
   //   {id: 1, firstName: 'John', lastName: 'Doe'},
   //   {id: 2, firstName: 'Brad', lastName: 'Traversy'},
