20240ec84fe95dfb640ec2a95db51e7bc27fe703
musaa-muhsen <musaamuhsen@gmail.com>
2021-02-11 17:51:43 +0000

    env added

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
 const path = require('path');
-//require('dotenv').config()
-
-//console.log(process.env)
+require('dotenv').config()
+const API_KEY = process.env.API_KEY;
+console.log(API_KEY)
 
 //app.get('/api/images', async (req, res) => {
 
@@ -13,16 +13,12 @@ app.get('/api/:word', async (req, res) => {
   //const API_KEY = process.env.API_KEY;
   const word = req.params.word;
   console.log(word);
-   
   // res.json(customers);
  // const imageURL = await fetch(`https://api.unsplash.com/search/photos?query=${word}&client_id=BgwLpezqvGtqHU5Q23sXjv0kJSebWvOoG_3fKA6ALOs`); 
-  const imageURL = await fetch(`https://api.unsplash.com/search/photos?per_page=30&query=${word}&client_id=BgwLpezqvGtqHU5Q23sXjv0kJSebWvOoG_3fKA6ALOs`); 
-
-   const data = await imageURL.json();
-   res.json(data)
+  const imageURL = await fetch(`https://api.unsplash.com/search/photos?per_page=30&query=${word}&client_id=${API_KEY}`); 
+  const data = await imageURL.json();
+  res.json(data)
   //console.log(images)
-
-
 });
 
 // serve static assets if in production 
